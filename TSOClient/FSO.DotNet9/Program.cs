22ce2ea48be16a493c3a52e8f12004e7abffb849
SegerEnd <gj.endendijk@gmail.com>
2025-09-18 17:32:28 +0200

    Add FSO.DotNet9 MonoGame project for .NET 9

---
+++ TSOClient/FSO.DotNet9/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace FSO.DotNet9
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            using var game = new FSO.DotNet9.Game1();
+            game.Run();
+            //if ((new FSOProgram()).InitWithArguments(args))
+            //    (new GameStartProxy()).Start(UseDX);
+        }
+    }
+}
