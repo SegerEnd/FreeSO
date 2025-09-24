16fea5bb7178c5732fc0aedc5149ec975cbdaf07
SegerEnd <gj.endendijk@gmail.com>
2025-09-24 23:51:22 +0200

    Changes, not yet working

--- TSOClient/FSO.iOS/Program.cs
+++ TSOClient/FSO.iOS/Program.cs
@@ -9,16 +9,25 @@ using System.Threading;
 using FSO.Files;
 using FSO.Client.UI.Panels;
 //using Ninject;
+#if MONOMAC
+using MonoMac.AppKit;
+using MonoMac.Foundation;
+#elif __IOS__ || __TVOS__
 using Foundation;
 using FSO.Client;
 using FSO.Common;
 using UIKit;
+#endif
 #endregion
 
 namespace FSOiOS
 {
+#if __IOS__ || __TVOS__
     [Register("AppDelegate")]
     class Program : UIApplicationDelegate
+#else
+	static class Program
+#endif
 	{
 
         public static Action<string> MainOrg;
@@ -41,7 +50,7 @@ namespace FSOiOS
 			FSOEnvironment.UIZoomFactor = iPad?1:2;
             FSOEnvironment.DPIScaleFactor = iPad ? 2 : 1;
             FSOEnvironment.TexCompress = false;
-            FSOEnvironment.TexCompressSupport = true;
+            FSOEnvironment.TexCompressSupport = false;
 
             FSOEnvironment.GameThread = Thread.CurrentThread;
             FSOEnvironment.Enable3D = true;
@@ -164,4 +173,3 @@ namespace FSOiOS
     }  
 #endif
 }
-
